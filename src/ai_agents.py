f27971d8753d179d73d035216eb3c446aff58920
Amir Mahdavieh <amir.mahdavieh79@gmail.com>
2026-06-01 15:54:56 +0200

    refactor: migrate agent SDK from OpenAI to Groq

--- src/ai_agents.py
+++ src/ai_agents.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
-from agents import Agent
+from agents import Agent, set_default_openai_client, set_tracing_disabled, set_default_openai_api
 from tools import execute_sql
+from openai import OpenAI, AsyncOpenAI
+import os
 
 load_dotenv()
 
-converter_agent = Agent(
-    name="Text to SQL converter",
-    instructions="""
-        You are a database assistant for SQLite.
+instructions = """
+    You are a database assistant for SQLite.
 
         Convert the user's request into a safe SELECT query, call the execute_sql tool, and return the result clearly.
 
@@ -16,8 +16,25 @@ converter_agent = Agent(
         - Never use DROP, DELETE, UPDATE, INSERT, ALTER, CREATE, or PRAGMA.
         - If the request is unsafe, return UNSAFE_QUERY.
         - Use only existing tables and columns.
-        """,
+"""
+
+'''converter_agent = Agent(
+    name="Text to SQL converter",
+    instructions=instructions,
     model="gpt-4o-mini",
     tools=[execute_sql]
+)'''
 
+set_tracing_disabled(True)
+groq = AsyncOpenAI(api_key=os.getenv("GROQ_API_KEY"), base_url="https://api.groq.com/openai/v1")
+model_name = "llama-3.3-70b-versatile"
+
+set_default_openai_client(groq)
+set_default_openai_api("chat_completions")
+
+converter_agent = Agent(
+    name="Text to SQL converter",
+    instructions=instructions,
+    model=model_name,
+    tools=[execute_sql] 
 )
